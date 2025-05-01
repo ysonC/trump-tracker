cff0106c39cdf5aa5f2d29fac233666c9a3b0631
Wyson Cheng <wyson002@gmail.com>
2025-05-01 15:02:17 +0100

    change to more stable version of scrapping

--- main.py
+++ main.py
@@ -1,37 +1,27 @@
-import time
-
-from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 
 TRUMP_URL = "https://truthsocial.com/@realDonaldTrump"
 
 
-def extract_from_html(html: str):
-    soup = BeautifulSoup(html, "html.parser")
-    posts = soup.select("div.break-words")
-    return [post.get_text(strip=True) for post in posts if post]
-
-
-def get_latest_post():
+def get_posts(number):
+    scrapped_post = []
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto(TRUMP_URL)
 
         page.wait_for_timeout(2000)
-        for _ in range(10):
+        for _ in range(number):
+            page.wait_for_timeout(2000)
             posts = page.locator("div.status__content-wrapper p.text-base")
             for i in range(posts.count()):
-                print("#########################")
-                print(posts.nth(i).inner_text())
-                print("#########################")
+                scrapped_post.append(posts.nth(i).inner_text())
             page.mouse.wheel(0, 1000)
 
-        # print(posts.all_inner_texts())
-        html = page.content()
         browser.close()
-        return extract_from_html(html)
+        return scrapped_post
 
 
 if __name__ == "__main__":
-    truth = get_latest_post()
+    truth = get_posts(30)
+    print(truth)
